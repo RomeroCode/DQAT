f78095d8f5eceebc9d60178dcdc99ec8ec03166f
Marcos Romero <marcosromero11@gmail.com>
2024-02-12 19:11:16 +0000

    Header processors, header kafka topics

---
+++ config/app_config.py
@@ -0,0 +1,5 @@
+# Application specific configuration
+
+EXPECTED_HEADERS = ['filename', 'timestamp', 'entry_id', 'temperature(c)', 'turbidity(ntu)',
+    'dissolvedoxygen(g/ml)', 'ph', 'ammonia(g/ml)', 'nitrate(g/ml)',
+    'population', 'fish_length(cm)', 'fish_weight(g)']
